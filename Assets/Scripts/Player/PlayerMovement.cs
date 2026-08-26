f1ef0e24fae9c086299d2797daebcf7d1b34ad62
matthychang97 <matthewchang97@gmail.com>
2026-08-26 20:27:25 +1000

    FPS-Gaming

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+    public float movementSpeed = 5f;
+
+    private float forwardInputValue;
+    private float strafeInputValue;
+
+    private CharacterController characterController;
+
+    void Awake ()
+    { 
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        forwardInputValue = Input.GetAxisRaw("Vertical");
+        strafeInputValue = Input.GetAxisRaw("Horizontal");
+        Movement();
+    }
+
+    void Movement()
+    {
+        Vector3 direction = (transform.forward * forwardInputValue + transform.right * strafeInputValue).normalized * movementSpeed * Time.deltaTime;
+
+        characterController.Move(direction);
+    }
+}
